499cf449ee177bdbc62ad5c741ca62de307e84eb
taniaceb <tatjana.cebatul@gmail.com>
2019-07-07 15:35:02 +0300

    New function. StringToInt Check is number is between two values (-9;9)

--- HW_Exercise1/Program.cs
+++ HW_Exercise1/Program.cs
@@ -8,19 +8,55 @@ namespace HW_Exercise1
         {
             Console.WriteLine("Iveskite viena skaiciu nuo -9 iki 9");
             string number = Console.ReadLine();
-            Console.WriteLine(CheckIsNumber(ref number));
+
+            //Patikrinti, ar buvo ivestas skaicius
+
+            if (number == "")
+            {
+                Console.WriteLine("Skaicius nebuvo ivestas ");
+            }
+            else
+            {
+                Console.WriteLine(CheckIsNumber(number));
+                Console.WriteLine( StringToInt(-9, 9, number));
+            }
+
+            
+            
 
         }
 
-        static bool CheckIsNumber(ref string a)
+        static bool StringToInt(int a, int b, string c)
+        {
+            bool d = false;
+            int num = Convert.ToInt32(c);
+            if ( num >= a && num <= b )
+            {
+                d = true;
+            }
+
+            return d;
+
+        }
+
+
+
+
+
+
+
+        static bool CheckIsNumber(string a)
         {
 
             bool check = true;
 
+            //Tikriname, 1-a simboli
 
             for (int i = 0; i < 1; i++)
             {
                 char simbol1 = a[i];
+
+                //Jei buvo ivestas tik vienas simbolis, jis g.b. tik 0,1,2,3,4,5,6,7,8,9
 
                 if (a.Length <= 1 && (simbol1 != '0' && simbol1 != '1' && simbol1 != '2' && simbol1 != '3' && simbol1 != '4' && simbol1 != '5' && simbol1 != '6' && simbol1 != '7' && simbol1 != '8' && simbol1 != '9'))
 
@@ -28,12 +64,14 @@ namespace HW_Exercise1
                     check = false;
                 }
 
+                //Jei buvo ivesta >1 simboli, pirmas simbolis <> 0
 
                 else if (a.Length > 1 && simbol1 == '0')
                 {
                     check = false;
                 }
 
+                //Jei pirmas simbolis - (minuso zenklas), 2- as simbolis gali buti tik 1,2,3,4,5,6,7,8,9
 
                 else if (a.Length > 1 && simbol1 == '-')
                 {
@@ -46,6 +84,7 @@ namespace HW_Exercise1
                         {
                             check = false;
                         }
+                        //Tikriname visus kitus simboilius, jei simboliu ilgis > 2
                         else
                         {
                             for (int k = 2; k < a.Length; k++)
